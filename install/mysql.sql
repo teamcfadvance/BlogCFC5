04458664049ce0b309d1e8f9956b40052d216a5d
cfjedimaster <cfjedimaster@b8309ce2-952a-0410-863c-8f1baba0c699>
2007-10-08 21:00:05 +0000

    updated install sql

--- install/mysql.sql
+++ install/mysql.sql
@@ -13,19 +13,19 @@
 
 /*!40101 SET NAMES utf8 */;
 CREATE TABLE `tblblogcategories` (
-  `categoryid` varchar(35) character set latin1 NOT NULL default '',
-  `categoryname` varchar(50) character set latin1 NOT NULL default '',
+  `categoryid` varchar(35) character set utf8 NOT NULL default '',
+  `categoryname` varchar(50) character set utf8 NOT NULL default '',
   `categoryalias` varchar(50) default NULL,
-  `blog` varchar(50) character set latin1 NOT NULL default '',
+  `blog` varchar(50) character set utf8 NOT NULL default '',
   PRIMARY KEY  (`categoryid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblblogcomments` (
-  `id` varchar(35) character set latin1 NOT NULL default '',
-  `entryidfk` varchar(35) character set latin1 default NULL,
-  `name` varchar(50) character set latin1 default NULL,
-  `email` varchar(50) character set latin1 default NULL,
-  `comment` text character set latin1,
+  `id` varchar(35) character set utf8 NOT NULL default '',
+  `entryidfk` varchar(35) character set utf8 default NULL,
+  `name` varchar(50) character set utf8 default NULL,
+  `email` varchar(50) character set utf8 default NULL,
+  `comment` text character set utf8,
   `posted` datetime default NULL,
   `subscribe` tinyint(1) default NULL,
   `website` varchar(255) default NULL,
@@ -34,18 +34,18 @@ CREATE TABLE `tblblogcomments` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblblogentries` (
-  `id` varchar(35) character set latin1 NOT NULL default '',
-  `title` varchar(100) character set latin1 NOT NULL default '',
-  `body` text character set latin1 NOT NULL,
+  `id` varchar(35) character set utf8 NOT NULL default '',
+  `title` varchar(100) character set utf8 NOT NULL default '',
+  `body` text character set utf8 NOT NULL,
   `posted` datetime NOT NULL default '0000-00-00 00:00:00',
-  `morebody` text character set latin1,
-  `alias` varchar(100) character set latin1 default NULL,
-  `username` varchar(50) character set latin1 default NULL,
-  `blog` varchar(50) character set latin1 NOT NULL default '',
+  `morebody` text character set utf8,
+  `alias` varchar(100) character set utf8 default NULL,
+  `username` varchar(50) character set utf8 default NULL,
+  `blog` varchar(50) character set utf8 NOT NULL default '',
   `allowcomments` tinyint(1) NOT NULL default '0',
-  `enclosure` varchar(255) character set latin1 default NULL,
+  `enclosure` varchar(255) character set utf8 default NULL,
   `filesize` int(11) unsigned default '0',
-  `mimetype` varchar(255) character set latin1 default NULL,
+  `mimetype` varchar(255) character set utf8 default NULL,
   `views` int(11) default NULL,
   `released` tinyint(1) default '0',
   `mailed` tinyint(1) default '0',
@@ -53,8 +53,8 @@ CREATE TABLE `tblblogentries` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblblogentriescategories` (
-  `categoryidfk` varchar(35) character set latin1 default NULL,
-  `entryidfk` varchar(35) character set latin1 default NULL
+  `categoryidfk` varchar(35) character set utf8 default NULL,
+  `entryidfk` varchar(35) character set utf8 default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblblogentriesrelated` (
@@ -73,15 +73,15 @@ CREATE TABLE `tblblogpages` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblblogsearchstats` (
-  `searchterm` varchar(255) character set latin1 NOT NULL default '',
+  `searchterm` varchar(255) character set utf8 NOT NULL default '',
   `searched` datetime NOT NULL default '0000-00-00 00:00:00',
-  `blog` varchar(50) character set latin1 NOT NULL default ''
+  `blog` varchar(50) character set utf8 NOT NULL default ''
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblblogsubscribers` (
-  `email` varchar(50) character set latin1 NOT NULL default '',
-  `token` varchar(35) character set latin1 NOT NULL default '',
-  `blog` varchar(50) character set latin1 default NULL,
+  `email` varchar(50) character set utf8 NOT NULL default '',
+  `token` varchar(35) character set utf8 NOT NULL default '',
+  `blog` varchar(50) character set utf8 default NULL,
   `verified` tinyint(1) NOT NULL default '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -94,25 +94,25 @@ CREATE TABLE `tblblogtextblocks` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblblogtrackbacks` (
-  `Id` varchar(35) character set latin1 NOT NULL default '',
-  `title` varchar(255) character set latin1 NOT NULL default '',
-  `blogname` varchar(255) character set latin1 NOT NULL default '',
-  `posturl` varchar(255) character set latin1 NOT NULL default '',
-  `excerpt` text character set latin1 NOT NULL,
+  `Id` varchar(35) character set utf8 NOT NULL default '',
+  `title` varchar(255) character set utf8 NOT NULL default '',
+  `blogname` varchar(255) character set utf8 NOT NULL default '',
+  `posturl` varchar(255) character set utf8 NOT NULL default '',
+  `excerpt` text character set utf8 NOT NULL,
   `created` datetime NOT NULL default '0000-00-00 00:00:00',
-  `entryid` varchar(35) character set latin1 NOT NULL default '',
-  `blog` varchar(50) character set latin1 NOT NULL default '',
+  `entryid` varchar(35) character set utf8 NOT NULL default '',
+  `blog` varchar(50) character set utf8 NOT NULL default '',
   PRIMARY KEY  (`Id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `tblusers` (
-  `username` varchar(50) character set latin1 default NULL,
-  `password` varchar(50) character set latin1 default NULL,
+  `username` varchar(50) character set utf8 default NULL,
+  `password` varchar(50) character set utf8 default NULL,
   `name` varchar(50) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-/*!40101 SET NAMES latin1 */;
+/*!40101 SET NAMES utf8 */;
 
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -135,7 +135,7 @@ CREATE INDEX blogEntriesRelated_relatedid ON
 tblblogentriesrelated(relatedid,entryid);
 CREATE INDEX blogTrackBacks_entryid ON tblblogtrackbacks(entryid);
 
-/*!40101 SET NAMES latin1 */;
+/*!40101 SET NAMES utf8 */;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
